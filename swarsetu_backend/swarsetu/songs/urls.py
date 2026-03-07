8f5028af3fd865f1fe35f737c0df7f98182c7c50
Saksham Raj <sakshamgrd09@gmail.com>
2026-03-07 12:50:25 +0530

    Restart

--- swarsetu_backend/swarsetu/songs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.song_list_create),
-    path("<int:id>/", views.song_detail),
-    path("translate/", views.translate),
-]
